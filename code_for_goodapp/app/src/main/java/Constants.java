e88b64c89dce6dc143d010c4bc37d025133df0a3
ShubhamThakur11 <shubamt426@gmail.com>
2018-07-08 08:44:48 +0530

    constants file added

---
+++ code_for_goodapp/app/src/main/java/Constants.java
@@ -0,0 +1,12 @@
+package com.example.shubhamkumar.code_for_good;
+
+/**
+ * Created by Shubham Kumar on 7/7/2018.
+ */
+
+public class Constants {
+    public static final String ROOT_URL="https://wayne-enterprises.000webhostapp.com/codeforgood/";
+    public static final String Admin_LOGIN = ROOT_URL+"farmer_login.php";
+    public static final String Control_LOGIN = ROOT_URL+"control_login.php";
+    public static final String EQUIPMENT_DETAILS=ROOT_URL+"equipment_details.php";
+}
